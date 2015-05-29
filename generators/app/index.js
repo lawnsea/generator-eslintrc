3a5ef425720960bf0c085ec1e85bfd2357136f09
Lon Ingram <lawnsea@gmail.com>
2015-05-29 14:05:39 -0500

    learn `--destination`
    To create an `.eslintrc` in a location other than the project root, pass a
    relative path via `--destination`.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,12 +1,35 @@
 'use strict';
+
+var path = require('path');
 var yeoman = require('yeoman-generator');
 
+var isAbsolute = path.isAbsolute ? path.isAbsolute : function (p) {
+    // polyfill of path.isAbsolute for node < 0.12.x
+    return path.resolve(p) === path.normalize(p);
+  };
+
 module.exports = yeoman.generators.Base.extend({
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+
+    this.option('destination', {
+      desc: 'directory to write .eslintrc in',
+      type: 'String',
+      defaults: './'
+    });
+  },
+
+  initializing: function () {
+    if (isAbsolute(this.options.destination)) {
+      this.env.error('destination must be a relative path');
+    }
+  },
+
   writing: {
     app: function () {
       this.fs.copy(
         this.templatePath('eslintrc'),
-        this.destinationPath('.eslintrc')
+        this.destinationPath(this.options.destination, '.eslintrc')
       );
     }
   }
--- test/test-app.js
+++ test/test-app.js
@@ -5,14 +5,32 @@ var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
 
 suite('eslintrc:app', function () {
-  suiteSetup(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .on('end', done);
+  suite('default behavior', function () {
+    suiteSetup(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .on('end', done);
+    });
+
+    test('creates .eslintrc', function () {
+      assert.file([
+        './.eslintrc'
+      ]);
+    });
   });
 
-  test('creates .eslintrc', function () {
-    assert.file([
-      '.eslintrc'
-    ]);
+  suite('--destination=foo/bar', function () {
+    suiteSetup(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withOptions({
+          destination: 'foo/bar'
+        })
+        .on('end', done);
+    });
+
+    test('creates foo/bar/.eslintrc', function () {
+      assert.file([
+        './foo/bar/.eslintrc'
+      ]);
+    });
   });
 });
