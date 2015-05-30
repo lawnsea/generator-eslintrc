a766952058275726f20af934d459d44395fb5268
Lon Ingram <lawnsea@gmail.com>
2015-05-30 11:50:16 -0500

    learn `--source`
    To use an existing file as the new `.eslintrc`, pass an absolute path or a path
    relative to the current working directory via `--source`.

--- test/test-app.js
+++ test/test-app.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var path = require('path');
 var assert = require('yeoman-generator').assert;
+var fs = require('fs');
 var helpers = require('yeoman-generator').test;
+var path = require('path');
+var appDir = path.join(__dirname, '../generators/app');
 
 suite('eslintrc:app', function () {
   suite('default behavior', function () {
     suiteSetup(function (done) {
-      helpers.run(path.join(__dirname, '../generators/app'))
+      helpers.run(appDir)
         .on('end', done);
     });
 
@@ -20,7 +22,7 @@ suite('eslintrc:app', function () {
 
   suite('--destination=foo/bar', function () {
     suiteSetup(function (done) {
-      helpers.run(path.join(__dirname, '../generators/app'))
+      helpers.run(appDir)
         .withOptions({
           destination: 'foo/bar'
         })
@@ -31,6 +33,30 @@ suite('eslintrc:app', function () {
       assert.file([
         './foo/bar/.eslintrc'
       ]);
+    });
+  });
+
+  suite('--source=foo/bar', function () {
+    var sourceText = '{ "rules": {} }';
+
+    suiteSetup(function (done) {
+      helpers.run(appDir)
+        .inDir(path.join(appDir, '../../tmp'), function (dir) {
+          fs.mkdirSync('foo');
+          fs.writeFileSync('foo/bar', sourceText);
+        })
+        .withOptions({
+          source: 'foo/bar'
+        })
+        .on('end', done);
+    });
+
+    test('writes foo/bar as .eslintrc', function () {
+      assert.file([
+        './.eslintrc'
+      ]);
+
+      assert.fileContent('./.eslintrc', sourceText);
     });
   });
 });
